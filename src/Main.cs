4ca81f3a0c73753a9a2e9b7947e2e4ad5737026f
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-03-23 23:40:26 -0300

    Update Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -4,8 +4,8 @@ using static CompositionLearning.src.Item.ItemManager;
 
 LoadItems();
 Item axe = GetItem("steel_axe");
-Tool axe_tool_component = axe.GetComponent<Tool>();
-Weapon axe_weapon_component = axe.GetComponent<Weapon>();
 Console.WriteLine(
-    $"Axe name: {axe.Name}, Axe durability: {axe_tool_component.Durability}, Axe damage: {axe_weapon_component.Damage}");
-
+    $"Axe name: {axe.Name}, " +
+    $"Axe durability: {axe.GetComponent<Tool>()?.Durability}, " +
+    $"Axe damage: {axe.GetComponent<Weapon>()?.Damage}"
+);
